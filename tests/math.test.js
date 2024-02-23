d6e712fbe31957dd659653729cb85761b62d9a0a
Antoine <poeuf.alexantoine@orange.fr>
2024-02-23 10:58:33 +0100

    wip

---
+++ tests/math.test.js
@@ -0,0 +1,11 @@
+test('adds 1 + 2 to equal 3', () => {
+    expect((1+ 2)).toBe(3);
+});
+
+test('adds 5 + 7 to equal 12', () => {
+    expect((5+ 7)).toBe(12);
+});
+
+test('adds -2 + 2 to equal 0', () => {
+    expect((-2+ 2)).toBe(0);
+});
